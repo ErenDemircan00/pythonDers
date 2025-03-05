fdf1ea31a58ed80773ca62bd937a4b0bc0f06492
Eren Demircan <154807775+ErenDemircan00@users.noreply.github.com>
2025-03-05 15:03:06 +0300

    Initial commit

---
+++ app.py
@@ -0,0 +1,4 @@
+print('merhaba benim adım {}'.format("eren"))
+print('merhaba benim adım {0},yasim {1}'.format("eren",21))
+print('merhaba benim adım {1},yasim {0}'.format("eren",21))
+print('merhaba benim adım {ad},yasim {yas}'.format(ad="eren",yas=21))
